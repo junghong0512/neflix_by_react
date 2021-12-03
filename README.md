fa67487cd4e7b66b15eb94375aa37c6e0157c6ed
junghong <junghong@ninewatt.com>
2021-12-03 10:28:06 +0900

    #14 Networking, using axios

--- README.md
+++ README.md
@@ -8,3 +8,14 @@ Learning React and ES6 by Building a Movie Discovery App.
 - [ ] TV Show
 - [ ] Search
 - [ ] Detail
+
+## API Verbs
+
+- [x] Now Playing (Movie)
+- [x] Upcoming (Movie)
+- [x] Top Rated (TV)
+- [x] Popular (TV)
+- [x] Airing Today (TV)
+- [x] TV Show Datail
+- [x] Movie Detail
+- [x] Search (Movie, TV)
---
+++ src/api.js
@@ -0,0 +1,41 @@
+import axios from 'axios';
+
+const api = axios.create({
+    baseURL: 'https://api.themoviedb.org/3/',
+    params: {
+        api_key: "965b1cb9d4f8aa5a05827a33693623c1",
+        language: "en-US"
+    }
+});
+
+export const movieApi = {
+    nowPlaying: () => api.get("movie/now_playing"),
+    upcoming: () => api.get("movie/upcoming"),
+    popular: () => api.get("movie/popular"),
+    movieDetail: id => api.get(`movie/${id}`, {
+        params: {
+            append_to_response: "videos"
+        }
+    }),
+    search: term => api.get("search/movie", {
+        params: {
+            query: encodeURIComponent(term)
+        }
+    })
+};
+
+export default tvApi = {
+    topRated: () => api.get("tv/top_rated"),
+    popular: () => api.get("tv/popular"),
+    airingToday: () => api.get("tv/airing_today"),
+    showDetail: id => api.get(`tv/${id}`, {
+        params: { 
+            append_to_response: "videos"
+        }
+    }),
+    search: term => api.get("search/tv", {
+        params: {
+            query: encodeURIComponent(term)
+        }
+    })
+}
